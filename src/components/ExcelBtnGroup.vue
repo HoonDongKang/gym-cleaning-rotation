7853a7115569e6e4cc02d8e5709fc8da108d58ca
HoonDongKang <d159123@naver.com>
2025-05-25 21:26:30 +0900

    feat: Add exceljs to read excel files

--- src/components/ExcelBtnGroup.vue
+++ src/components/ExcelBtnGroup.vue
@@ -22,16 +22,43 @@
 
 <script setup>
 import { ref } from 'vue';
+import ExcelJS from 'exceljs';
 
 const selectedFile = ref(null);
 const fileInput = ref(null);
+const excelData = ref([]);
 
 const uploadFile = () => {
   fileInput.value.click();
 };
 
-const handleFileChange = (event) => {
-  const file = event.target.file[0];
+const handleFileChange = async (event) => {
+  console.log(event);
+  const file = event.target.files[0];
+  if (!file) return;
   selectedFile.value = file;
+
+  try {
+    const workbook = new ExcelJS.Workbook();
+    const arrayBuffer = await file.arrayBuffer();
+    await workbook.xlsx.load(arrayBuffer);
+
+    const workSheet = workbook.getWorksheet(1);
+    const data = [];
+
+    workSheet.eachRow((row, rowNumber) => {
+      const rowData = [];
+      row.eachCell((cell, colNumber) => {
+        rowData.push(cell.value);
+      });
+      data.push(rowData);
+    });
+
+    excelData.value = data;
+
+    console.log(excelData.value);
+  } catch (e) {
+    console.error('파일 읽기 오류', error);
+  }
 };
 </script>
