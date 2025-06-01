390eea78d46141a2330028b38dc399207d8f2fc3
HoonDongKang <d159123@naver.com>
2025-06-01 15:17:34 +0900

    feat: Add schedule test route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
-import MainView from '@/views/MainView.vue';
 import { createRouter, createWebHistory } from 'vue-router';
+import MainView from '@/views/MainView.vue';
+import CleaningSchedule from '@/components/CleaningSchedule.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,11 +10,11 @@ const router = createRouter({
       name: 'main',
       component: MainView,
     },
-    // {
-    //   path: '/schedule',
-    //   name: 'schedule',
-    //   component: CleaningSchedule,
-    // },
+    {
+      path: '/schedule',
+      name: 'schedule',
+      component: CleaningSchedule,
+    },
   ],
 });
 
